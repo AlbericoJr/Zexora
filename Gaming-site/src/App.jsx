35b05c0632c378dcd88bca4745e71d040093039f
AlbericoJr <40373171+AlbericoJr@users.noreply.github.com>
2026-03-16 21:14:36 -0300

    Nexora Gaming - Projeto iniciado - Nav feita

--- Gaming-site/src/App.jsx
+++ Gaming-site/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 
 import Nav from './Component/Nav/Nav'
@@ -16,4 +15,4 @@ export default App
 
 
 // https://nexora-gaming-reactjs.vercel.app/#
-// YouTube: https://youtu.be/hgxjf2sFDvY?si=yxDYsO8-OqVWJoTo 6:40
+// YouTube: https://youtu.be/hgxjf2sFDvY?si=yxDYsO8-OqVWJoTo 20:30
--- Gaming-site/src/Component/Nav/Nav.jsx
+++ Gaming-site/src/Component/Nav/Nav.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import { useRef } from "react"
 import NavCss from "./Nav.module.css"
 
 export default function Nav() {
+
+  const menubar = useRef();
+
+  const openMenu = () => {
+    menubar.current.classList.toggle(NavCss.openMenu);
+  }
+
   return (
     <div className={NavCss.nav}>
-      <div className={NavCss.menu}>
-        <li>
-          <a href="#">Home</a>
-          <a href="#">Sobre</a>
-          <a href="#">Projetos</a>
-          <a href="#">Equipe</a>
-          <a href="#">Depoimento</a>
-          <a href="#">Contato</a>
-        </li>
+      <div className={NavCss.menu} ref={menubar}>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Projetos</a></li>
+        <li><a href="#">Equipe</a></li>
+        <li><a href="#">Depoimento</a></li>
+        <li><a href="#">Contato</a></li>
       </div>
 
       <div className={NavCss.bars}>
-        <i className={`${NavCss.nav_icon} ri-menu-2-fill`}></i>
+        <i className={`${NavCss.nav_icon} ri-menu-2-fill`} onClick={openMenu}></i>
+      </div>
+
+      <div className={NavCss.logo}>
+        <a href="#">Zex<span>ora</span></a>
+      </div>
+      
+      <div className={NavCss.right_menu}>
+        <h2>INFO@EXAMPLE.COM</h2>
+        <div className={NavCss.nav_icon}>
+          <i className={`${NavCss.nav_icon} ri-search-2-line`}></i>
+        </div>
       </div>
     </div>
   )
